8e3282c178de85893d6a67d942e68c511bb5d4a3
Dmytro <d.y.afelchonok@student.khai.edu>
2025-12-09 22:31:13 +0200

    add express router, middleware, error handling, product by id route

--- error.middleware.js
+++ error.middleware.js
@@ -1,12 +1,20 @@
+const errorResponder = (err, request, response, next) => {
+   response.header("Content-Type", 'application/json')
+   response.status(err.statusCode).send(err.message)
+}
+module.exports = { errorResponder };
+
 const express = require('express')
 const productRoutes = require('./product.routes');
 const { logRequest } = require('./middleware');
+const { errorResponder } = require('./error.middleware');
 
 const app = express();
 const PORT = 3000;
 
 app.use(logRequest);
 app.use(productRoutes);
+app.use(errorResponder);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
