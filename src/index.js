acba79e5ec68798ffb0be6dc629ba305f5063059
Dmytro <d.y.afelchonok@student.khai.edu>
2026-04-20 22:47:14 +0300

    create entity

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const productRoutes = require('./product.routes');
 const { logRequest } = require('./middleware');
+const bodyParser = require('body-parser');
+const userRouter = require('./user/user.router');
 
 const app = express();
 const PORT = 3000;
 
 app.use(logRequest);
 app.use(productRoutes);
+app.use(bodyParser.json());
+app.use(userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
